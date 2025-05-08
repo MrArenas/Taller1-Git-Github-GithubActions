89276f1e4843e39e777add3250fe2cc616765b2d
Felipe <fearenas@unal.edu.co>
2025-05-07 19:21:36 -0500

    Integración del flujo completo de ejecucion en main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Integracion de las funciones en un solo archivo
+from estudiantes import registro
+
+if __name__ == "__main__":
+    ruta_csv = "estudiantes.csv"
+    estudiantes = registro.cargar_estudiantes(ruta_csv)
+    registro.mostrar_estudiantes_tabla(estudiantes)
+    promedio = registro.promedio_estudiantes(estudiantes)
+    print(f"Promedio de notas: {promedio:.2f}")
